c71fb2cd05c6e54a47f314752a79128655fa7f7c
Jorge Kunrath <jorgeks95@gmail.com>
2020-09-13 01:59:50 -0300

    object to json + asyn await + real paragraphs

--- 3-pagina-com-layout/script.js
+++ 3-pagina-com-layout/script.js
@@ -1,46 +1,9 @@
-const data = [
-  {
-    title: `Ele tem um mecanismo de briga mais inteligente que o seu`,
-    subtitle: `Praticamente um ninja`,
-    content: `Quando a gente está discutindo com alguém é muito comum elevar a voz para se fazer ouvir, não é verdade? Bom, no mundo animal é preciso um pouco mais. Os gatos inflam os pelos e se curvam para parecerem maiores e dessa forma, seu inimigo deve temê-lo e fugir.
+(async function() {
+  const response = await fetch("./data.json")
+  const data = await response.json()
+  await data.forEach(createCard)
+}())
 
-    Os gatos criados em casa fazem isso mesmo quando não há perigo iminente, pelo menos não para nós. Ao ouvir um barulho diferente, ao ver algo fora do contexto do seu cotidiano, é provável que façam esse movimento. Afinal, é melhor prevenir do que ser surpreendido, não é verdade?`,
-    img: {
-      title: "Fight Cat!",
-      src: "./images/fight.gif"
-    }
-  },
-  {
-    title: `Ele poderia ser um agente da CIA`,
-    subtitle: `Um bem fofinho :3`,
-    content: `Se você acha que os gatos escondem suas fezes porque são muito limpinhos, você está certo. No entanto, há mais por trás dessa ação. Eles enterram suas fezes para não serem percebidos pelos seus inimigos naturais e também para que sua presa não seja capaz de perceber sua presença.
-
-    Isso é algo tão nato nos felinos, que assim que começam a andar já buscam a caixinha de areia e quando estão na natureza, buscam a terra mesmo. Fala a verdade, dessa você não sabia, não é?`,
-    img: {
-      title: "Gatos da Cia",
-      src: "./images/cia.gif"
-    }
-  },
-  {
-    title: `Ele é um saltador nato`,
-    subtitle: `Quase sempre...`,
-    content: `Os felinos são saltadores natos, podendo alcançar até cinco vezes a sua altura. É claro que às vezes pode acontecer uma pequena falha técnica, mas quem nunca?
-    Os gatos têm 30 vértebras, enquanto os humanos têm 24. Seus ouvidos têm a capacidade de dizer ao corpo onde ele está em relação ao chão, mesmo quando está de cabeça para baixo. Isso também é um facilitador nas descidas, proporcionando que ele caia sempre de pé.
-    
-    Mas atenção, mesmo assim é preciso ter cuidado: gatos não têm 7 vidas e podem se machucar seriamente nas quedas.`,
-    img: {
-      title: "Gato saltando banheira",
-      src: "./images/jump.gif"
-    }
-  },
-]
-
-
-// big loop
-data.map(createCard)
-
-
-// main function
 function createCard(item) {
 
   // desestrutura e armazena em constantes
@@ -48,26 +11,28 @@ function createCard(item) {
 
   // CREATE ELEMENTS
     // card
-    const Article = document.createElement("ARTICLE")
+    const Article = document.createElement("article")
     Article.classList.add("card")
 
     // title
-    const Title = document.createElement("H1")
+    const Title = document.createElement("h1")
     Title.innerText = title
     Article.appendChild(Title)
 
     // subtitle
-    const Subtitle = document.createElement("H2")
+    const Subtitle = document.createElement("h2")
     Subtitle.innerText = subtitle
     Article.appendChild(Subtitle)
 
     // content
-    const Content = document.createElement("P")
-    Content.innerText = content
-    Article.appendChild(Content)
+    content.split(/\n/).forEach(paragraph => {
+      const Content = document.createElement("p")
+      Content.innerText = paragraph
+      Article.appendChild(Content)
+    });
 
     // img
-    const Img = document.createElement("IMG")
+    const Img = document.createElement("img")
     Img.src = img.src
     Img.title = img.title
     Img.alt = img.title
